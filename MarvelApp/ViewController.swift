d5f75773db9dfdb1103303fd5f903844585a2755
rafaelgonzalez <rafael.glez@gmail.com>
2024-04-30 17:53:31 -0600

    Initial Commit

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Rafael González on 30/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
