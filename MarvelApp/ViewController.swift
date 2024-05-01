b9aaf20eac87564100c7368446ade5745c6da7a1
rafaelgonzalez <rafael.glez@gmail.com>
2024-05-01 13:05:12 -0600

    Initial version

--- MarvelApp/ViewController.swift
+++ MarvelApp/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var keyLoader = KeyLoader.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(keyLoader.getAPIParams())
+        print(keyLoader.getQueryString())
     }
-
-
 }
-
